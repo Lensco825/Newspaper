86d3a28a151d09bb53aa520c556dd2d7777d2170
abit salihu <abiti.salihu@gmail.com>
2022-10-31 12:31:10 +0100

    fix the toggle button and more of the overflow of the website

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const body = document.querySelector('body');
 
 function change() {
-  document.body.classList.add('change');
+  document.body.classList.toggle('change');
+  document.querySelector(".main-part").classList.toggle("b-right")
 }
